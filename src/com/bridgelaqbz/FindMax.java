c89440c6e3de875c2d0a7c1583c20429b6373f97
jayant <jayantparmar420@gmail.com>
2022-03-22 17:16:40 +0530

    added the refactor

--- src/com/bridgelaqbz/FindMax.java
+++ src/com/bridgelaqbz/FindMax.java
@@ -3,10 +3,16 @@ package com.bridgelaqbz;
 public class FindMax {
     public static void main(String[] args) {
         System.out.println("welcome to the generic max problem solving statement");
-//one = Banana ,two = Apple ,three = Peach;  (Maximum Number is at 3rd Position).
-        System.out.println(maximumStringMethod("Banana", "Apple", "Peach"));
+        //one = 86 ,two = 36,three = 52;  (Maximum Number is at 1st Position).
+        System.out.println(maximumMethod(86, 36, 52));
+        //one = 8.31f ,two = 4.31f,three = 5.99f;  (Maximum Number is at 1st Position).
+        System.out.println(maximumMethod("8.31f", "4.31f", "5.99f"));
+        //one = Peach ,two = Banana ,three = Apple;  (Maximum Number is at 1st Position).
+        System.out.println(maximumMethod("Peach", "Banana", "Apple"));
     }
-    public static <T extends Comparable<T>> T maximumStringMethod(T one, T two, T three) {  //Return type of this method is <T> i.e Generic type with 3 parameters.
+
+    //Generic Method.
+    public static <T extends Comparable<T>> T maximumMethod(T one, T two, T three) {  //Return type of this method is <T> i.e Generic type with 3 parameters.
 
         T maximum = one;                   //1 : Initially we assume that 'maximum' is 'one'.
 
@@ -23,8 +29,8 @@ public class FindMax {
 
     }
 
-}
 
+}
 
 
 
