3bc4faf3ec9d5eca4838cf4cb39a31cacaa40e7f
Jayant Singh Parmar <99529680+jayantSparmar@users.noreply.github.com>
2022-03-24 17:03:40 +0530

    Update ArrayPrint.java

--- src/com/bridgelaqbz/ArrayPrint.java
+++ src/com/bridgelaqbz/ArrayPrint.java
@@ -1,41 +1,77 @@
 package com.bridgelaqbz;
 
-public class ArrayPrint{
-        public static <T extends Comparable<T>> void genericSortMethod(T[] arr) {   // Generic method extending to Comparable with generic array as parameter.
+public class ArrayPrint{<T extends Comparable<T>> {
+    /*
+     * Find Maximum Problem using Generic.
+     */
 
-            T temp;
-            for(int i = 0 ; i<arr.length;i++) {                       //Sorting logic
-                for(int j = 0 ;j<arr.length-1;j++) {
+    //Global variables
+    T x, y, z;
 
-                    if(arr[j].compareTo(arr[j+1])<0) {
-                        temp = arr[j];
-                        arr[j]= arr[j+1];
-                        arr[j+1]=temp;
+    public ArrayPrint(T x, T y, T z) {
+        //Assigning values
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
-                    }
+    public static void main(String[] args) {
+        //Local variables.
+        Integer num1 = 27;
+        Integer num2 = 50;
+        Integer num3 = 97;
 
-                }
-            }for(int i = 0 ; i<arr.length;i++) {
-                System.out.println(arr[i]);
-            }
+        Float number1 = 98.4f;
+        Float number2 = 67.9f;
+        Float number3 = 56.4f;
 
+        String name1 = "Jayant";
+        String name2 = "Singh";
+        String name3 = "Parmar";
+
+        /*
+         * Calling constructor through object creation.
+         * Calling maximum method through object reference variable.
+         */
+        ArrayPrint<Integer> printMaxInteger = new ArrayPrint<>(num1, num2, num3);
+        printMaxInteger.maximum();
+
+        ArrayPrint<Float> printMaxFloat = new ArrayPrint<>(number1, number2, number3);
+        printMaxFloat.maximum();
+
+        ArrayPrint<String> printMaxString = new ArrayPrint<>(name1, name2, name3);
+        printMaxString.maximum();
+
+    }
+
+    /*
+     * Generic method to find the maximum value.
+     */
+    public static <T extends Comparable<T>> T testMax(T x, T y, T z) {
+
+        T max = x;
+        if (y.compareTo(max) > 0) {
+            max = y;
         }
-
-
-        public static void main(String[] args) {                //Main Method
-
-            Integer[] intArray = {53,23,65,25,85,63};               //Integer array sort
-            genericSortMethod(intArray);
-
-            System.out.println(" ");
-
-            Double[] doubleArray = {7.6,2.5,4.5,9.0,1.6};           //Double array sort
-            genericSortMethod(doubleArray);
-
-            System.out.println(" ");
-
-            String[] stringArray = {"Peach","Apple","Orange","Banana","Mango"};   //String array sort
-            genericSortMethod(stringArray);
+        if (z.compareTo(max) > 0) {
+            max = z;
         }
+        printMax(x, y, z, max);
+        return max;
+    }
+
+    /*
+     * Printing the maximum value
+     */
+    public static <T> void printMax(T x, T y, T z, T maximum) {
+        System.out.printf("Max of %s, %s and %s is %s \n", x, y, z, maximum);
+    }
+
+    public T maximum() {
+        //calling tsetMax method
+        return ArrayPrint.testMax(x, y, z);
+    }
 }
+
+        
 
